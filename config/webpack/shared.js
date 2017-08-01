6ff31b522f273c19af19a6a070a87038cbb7507a
Brendon Harris <brendon.harris9@gmail.com>
2017-08-01 16:49:05 -0500

    webpack configuration

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -1,3 +1,63 @@
+// // Note: You must restart bin/webpack-dev-server for changes to take effect
+//
+// /* eslint global-require: 0 */
+// /* eslint import/no-dynamic-require: 0 */
+//
+// const webpack = require('webpack')
+// const { basename, dirname, join, relative, resolve } = require('path')
+// const { sync } = require('glob')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// const ManifestPlugin = require('webpack-manifest-plugin')
+// const extname = require('path-complete-extname')
+// const { env, paths, output, loadersDir } = require('./configuration.js')
+//
+// const extensionGlob = `**/*{${paths.extensions.join(',')}}*`
+// const packPaths = sync(join(paths.source, paths.entry, extensionGlob))
+//
+// module.exports = {
+//   entry: packPaths.reduce(
+//     (map, entry) => {
+//       const localMap = map
+//       const namespace = relative(join(paths.source, paths.entry), dirname(entry))
+//       localMap[join(namespace, basename(entry, extname(entry)))] = resolve(entry)
+//       return localMap
+//     }, {}
+//   ),
+//
+//   output: {
+//     filename: '[name].js',
+//     path: output.path,
+//     publicPath: output.publicPath
+//   },
+//
+//   module: {
+//     rules: sync(join(loadersDir, '*.js')).map(loader => require(loader))
+//   },
+//
+//   plugins: [
+//     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
+//     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
+//     new ManifestPlugin({
+//       fileName: paths.manifest,
+//       publicPath: output.publicPath,
+//       writeToFileEmit: true
+//     })
+//   ],
+//
+//   resolve: {
+//     extensions: paths.extensions,
+//     modules: [
+//       resolve(paths.source),
+//       resolve(paths.node_modules)
+//     ]
+//   },
+//
+//   resolveLoader: {
+//     modules: [paths.node_modules]
+//   }
+// }
+
+
 // Note: You must restart bin/webpack-dev-server for changes to take effect
 
 /* eslint global-require: 0 */
@@ -9,16 +69,17 @@ const { sync } = require('glob')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const ManifestPlugin = require('webpack-manifest-plugin')
 const extname = require('path-complete-extname')
-const { env, paths, output, loadersDir } = require('./configuration.js')
+const { env, settings, output, loadersDir } = require('./configuration.js')
 
-const extensionGlob = `**/*{${paths.extensions.join(',')}}*`
-const packPaths = sync(join(paths.source, paths.entry, extensionGlob))
+const extensionGlob = `**/*{${settings.extensions.join(',')}}*`
+const entryPath = join(settings.source_path, settings.source_entry_path)
+const packPaths = sync(join(entryPath, extensionGlob))
 
 module.exports = {
   entry: packPaths.reduce(
     (map, entry) => {
       const localMap = map
-      const namespace = relative(join(paths.source, paths.entry), dirname(entry))
+      const namespace = relative(join(entryPath), dirname(entry))
       localMap[join(namespace, basename(entry, extname(entry)))] = resolve(entry)
       return localMap
     }, {}
@@ -38,21 +99,20 @@ module.exports = {
     new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(env))),
     new ExtractTextPlugin(env.NODE_ENV === 'production' ? '[name]-[hash].css' : '[name].css'),
     new ManifestPlugin({
-      fileName: paths.manifest,
       publicPath: output.publicPath,
       writeToFileEmit: true
     })
   ],
 
   resolve: {
-    extensions: paths.extensions,
+    extensions: settings.extensions,
     modules: [
-      resolve(paths.source),
-      resolve(paths.node_modules)
+      resolve(settings.source_path),
+      'node_modules'
     ]
   },
 
   resolveLoader: {
-    modules: [paths.node_modules]
+    modules: ['node_modules']
   }
 }
